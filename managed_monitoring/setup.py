9456419fdbf9eeda3d7e909205ffce586f56f3d6
Madda <nobody@nowhere.local>
2020-04-07 17:05:27 +0100

    Disable project

--- managed_monitoring/setup.py
+++ managed_monitoring/setup.py
@@ -4,6 +4,6 @@ setup(
     name='cloudify-managed-monitoring-plugin',
     version='0.1.0',
     packages=['cloudify_managed_monitoring'],
-    install_requires=['cloudify-plugins-common>=3.3.1',
-                      'Jinja2==2.10'],
+#    install_requires=['cloudify-plugins-common>=3.3.1',
+#                      'Jinja2==2.10'],
 )
--- snmp_agent/setup.py
+++ snmp_agent/setup.py
@@ -4,5 +4,5 @@ setup(
     name='cloudify-agent-snmp-plugin',
     version='0.2.1',
     packages=['cloudify_agent_snmp'],
-    install_requires=['cloudify-plugins-common>=3.3.1'],
+#    install_requires=['cloudify-plugins-common>=3.3.1'],
 )
